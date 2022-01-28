29666932d28e903d30e380ee09b44502f5ae83bc
Martix <66840932+MartixInTheMatrix@users.noreply.github.com>
2022-01-28 13:10:56 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Intents = exports.Event = exports.Command = exports.Client = void 0;
-exports.Client = require('./@discord.ts/Client').Client;
-exports.Command = require('./@discord.ts/Command').Command;
-exports.Event = require('./@discord.ts/Event').Event;
-exports.Intents = require('./@discord.ts/Intent').Intents;
-module.exports = require('discord.js');
